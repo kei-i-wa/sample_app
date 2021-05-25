9204517126da0a200342f548d82f6bb57201f5a2
EC2 Default User <ec2-user@ip-172-31-36-222.ec2.internal>
2021-05-25 09:11:58 +0000

    [finish]2-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,26 +4,22 @@ class TodolistsController < ApplicationController
     @list = List.new
   end
 
-  # 以下を追加
   def create
-    # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # ３. トップ画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
 
   def index
-    @lists=List.all
+    @lists = List.all
   end
 
   def show
-    @list=List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   def edit
-    @list=List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   def update
@@ -32,9 +28,14 @@ class TodolistsController < ApplicationController
     redirect_to todolist_path(list.id)
   end
 
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path  # 投稿一覧画面へリダイレクト  end
+  end
+
   private
-  # ストロングパラメータ
   def list_params
-     params.require(:list).permit(:title, :body, :image)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
